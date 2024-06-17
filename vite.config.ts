d1e45a5fdd8c3f8e9e9c8b8c4fa510d33cae3f25
chigayuki <107536212+chikinaN@users.noreply.github.com>
2024-06-17 13:52:39 +0900

    Initial commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,29 @@
+import { copyFileSync } from "node:fs";
+import { join } from "node:path";
+
 import { vitePlugin as remix } from "@remix-run/dev";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
+  base: "/roulette-app/",
   plugins: [
     remix({
+      ssr: false,
+      basename: "/roulette-app/",
+      buildEnd(args) {
+        if (!args.viteConfig.isProduction) return;
+        const buildPath = args.viteConfig.build.outDir;
+        copyFileSync(
+          join(buildPath, "index.html"),
+          join(buildPath, "404.html"),
+        );
+      },
       future: {
         v3_fetcherPersist: true,
         v3_relativeSplatPath: true,
         v3_throwAbortReason: true,
-      },
+      }
     }),
     tsconfigPaths(),
   ],
